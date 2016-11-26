834ade75a8dc9612386ea6242381f9f9078d7f75
Matz Larsson <matzlarsson94@gmail.com>
2016-11-26 14:12:35 +0200

    Trying hello example

--- app.js
+++ app.js
@@ -37,12 +37,14 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //res.locals.message = err.message;
+  //res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  //res.status(err.status || 500);
+  //res.render('error');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
 });
 
 module.exports = app;
