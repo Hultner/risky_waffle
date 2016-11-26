b63d91af77d493c25ffaf543ef85d92dd34e677d
Matz Larsson <matzlarsson94@gmail.com>
2016-11-26 17:09:50 +0200

    Added backend communication

--- public/src/client.js
+++ public/src/client.js
@@ -1,1 +1,29 @@
 start(true);
+
+
+function sendDataToMatchmaking(page, data, doneFunc, errorFunc){
+    $.post(window.location.hostname + '/matchmaking/' page, data).done(doneFunc).error(errorFunc);
+}
+
+function getDataFromMatchmaking(page, data, doneFunc, errorFunc){
+    $.get(window.location.hostname + '/matchmaking/' page, data).done(doneFunc).error(errorFunc);
+}
+
+function registerForVideo(uuid, callback){
+    sendDataToMatchmaking('registerClient', {'uuid':uuid}, undefined, function(error){
+        console.log('Problem contacting bacckend');
+        callback({'error':true});
+    });
+}
+
+function findBusStop(callback){
+    getDataFromMatchmaking('findBusStop', {}, callback, function(error){
+        callback({'error':true, 'e':error});
+    });
+}
+
+function setInactive(uuid, callback){
+    sendDataToMatchmaking('setStatus', {'uuid':uuid, 'status':'inactive'}, undefined, function(error){
+        callback({'error':true, 'e':error})
+    });
+}
