308edacfaf35e2efa2c2b24fdd45d96646ec35a0
Micro <maglars2@gmail.com>
2016-11-26 16:03:46 +0100

    Change name from matchmaking to busstop

--- app.js
+++ app.js
@@ -6,17 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var video = require('./routes/video');
-var matchmaking = require('./routes/matchmaking');
+var busStop = require('./routes/busStop');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,8 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/video', video);
-app.use('/matchmaking', matchmaking);
+app.use('/busStop', busStop);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
