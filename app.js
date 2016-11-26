486923ca43621e0ff53af15087ceafd578297d69
Matz Larsson <matzlarsson94@gmail.com>
2016-11-26 14:27:01 +0200

    Now it works!

--- app.js
+++ app.js
@@ -37,14 +37,12 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  //res.locals.message = err.message;
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  //res.status(err.status || 500);
-  //res.render('error');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('app');
+var app = require('./app');
 var debug = require('debug')('risky-waffle:server');
 var http = require('http');
 
