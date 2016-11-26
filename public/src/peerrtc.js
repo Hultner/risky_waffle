03072d169d98db715e3c1e855fa30aaacba277e5
Alexander Hultnér <ahultner@gmail.com>
2016-11-26 22:52:49 +0200

    Disabling audio as our idea builds on a video only concept

--- public/src/peerrtc.js
+++ public/src/peerrtc.js
@@ -21,7 +21,7 @@ peer.on('error', function(err){
 // Click handlers setup
 $(function(){
 
-  navigator.getUserMedia({audio: true, video: true}, function(stream){
+  navigator.getUserMedia({audio: false, video: true}, function(stream){
     // Set your video displays
     $('#my-video').prop('src', URL.createObjectURL(stream));
     window.localStream = stream;
