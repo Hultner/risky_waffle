b5502f248b94240ebe830e244055616d758078c0
Lisa Lipkin <lipkin@student.chalmers.se>
2016-11-26 13:40:34 +0100

    Added some database functionallity
    remove
    indexOf

--- modules/db.js
+++ modules/db.js
@@ -6,7 +6,7 @@ var fs = require('fs');
  * Returns the data from db.json
  */
 exports.read = function() {
-  var data = fs.readFileSync('../db/db.json', 'utf-8');
+  var data = JSON.parse(fs.readFileSync('../db/db.json', 'utf-8'));
   return data;
 };
 
@@ -14,7 +14,26 @@ exports.read = function() {
  * Writes data from db.json
  */
 exports.write = function(dbData) {
-  fs.writeFileSync('../db/db.json', dbData, 'utf-8');
+  fs.writeFileSync('../db/db.json', JSON.stringify(dbData), 'utf-8');
 };
 
+exports.remove = function(ip){
+  //remove
+    var database = exports.read();
+    var indexOfIp = indexOf(database, ip);
+    database = database.splice(indexOfIp, 1);
+    exports.write(database);
+};
+
+function indexOf( db, ip){
+    var count = 0;
+    while(db.length--){
+        if (db[count].ip == ip) {
+            return count;
+        } else{
+            count ++;
+        }
+    }
+    return -1;
+}
 module.exports = exports;
